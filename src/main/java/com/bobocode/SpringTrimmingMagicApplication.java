fe718aac512b0b43d11578160be20e81bcff9ab4
OKharchenko <OKharchenko@luxoft.com>
2022-07-18 22:02:23 +0300

    Initial commit

---
+++ src/main/java/com/bobocode/SpringTrimmingMagicApplication.java
@@ -0,0 +1,13 @@
+package com.bobocode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrimmingMagicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTrimmingMagicApplication.class, args);
+    }
+
+}
